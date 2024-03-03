ee6781fe0c5c1c9df540bfb9fd3108dd645cc672
RuCodinger <102660455+rucodinger@users.noreply.github.com>
2024-03-03 22:19:13 +0300

    Add form post to server
    Contact form soon!

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_page, name='index'),
     path('/', views.home_page, name='index'),
     path('courses/', views.courses_page, name='courses'),
+    path('contact/', views.contact_view, name='contact'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,7 @@
+from django.core.mail import send_mail
+from django.http import JsonResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -7,3 +10,20 @@ def home_page(request):
 
 def courses_page(request):
     return render(request, 'courses.html')
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+            print(name, email, message)
+            return render(request, 'form_success.html')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html')
+
+# def contact_view(request):
+#     return render(request, 'contact.html')
