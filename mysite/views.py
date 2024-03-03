9fd1270a7337a49a40b781ba0ff31540e9ed190e
RuCodinger <102660455+rucodinger@users.noreply.github.com>
2024-03-03 23:56:01 +0300

    remake form success page
    new remake soon

--- mysite/views.py
+++ mysite/views.py
@@ -14,17 +14,12 @@ def courses_page(request):
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        if form.is_valid():
-            # smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-            # smtpObj.starttls()
-            # smtpObj.login('vadikschool248@gmail.com', 'vadyusha5')
 
+        if form.is_valid():
             name = request.POST.get('name')
             email = request.POST.get('email')
             message = request.POST.get('message')
 
-            # smtpObj.sendmail('vadikschool248@gmail.com', email, f'Hello, {name}! {message}')
-            # smtpObj.quit()
             send_mail(
                 "Welcome!",
                 f'{name} is new user! His/her email: {email} \nMessage:\n{message}',
@@ -34,6 +29,4 @@ def contact_view(request):
             )
 
             return render(request, 'form_success.html')
-    else:
-        form = ContactForm()
     return render(request, 'contact.html')
