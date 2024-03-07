6bb90a9b7d1ba6e54f602470a0448380d757daf8
RuCodinger <102660455+rucodinger@users.noreply.github.com>
2024-03-07 22:25:33 +0300

    Some changes in frontend
    [-] Deleted courses_styles.css
    [/] Added fixed footer in styles.css
    [/] Chanhed some text in index.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='index'),
-    path('/', views.home_page, name='index'),
     path('courses/', views.courses_page, name='courses'),
     path('contact/', views.contact_view, name='contact'),
 ]
