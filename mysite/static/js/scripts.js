d140c2045792e0221eda0061a1904848e84c4432
RuCodinger <102660455+rucodinger@users.noreply.github.com>
2024-03-25 00:06:36 +0300

    Optimized course search

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -1,20 +1,20 @@
 function searchCourses() {
-    var input = document.getElementById('courseSearch').value.toLowerCase();
-    var cards = document.querySelectorAll('.card');
+    const input = document.getElementById('courseSearch').value.toLowerCase();
+    const cards = document.querySelectorAll('.card');
 
-    cards.forEach(function(card) {
-        var courseTitle = card.querySelector('.card-title').innerText.toLowerCase();
-        var courseDescription = card.querySelector('.card-text').innerText.toLowerCase();
-        if (courseTitle.includes(input) || courseDescription.includes(input)) {
-            card.parentElement.style.display = 'block';
-        } else {
-            card.parentElement.style.display = 'none';
+
+    cards.forEach(
+        function (card) {
+            const courseTitle = card.querySelector('.card-title').innerText.toLowerCase();
+            const courseDescription = card.querySelector('.card-text').innerText.toLowerCase();
+
+            if (courseTitle.includes(input) || courseDescription.includes(input)) card.parentElement.style.display = 'block';
+            else card.parentElement.style.display = 'none';
         }
-    });
+    );
+    console.log("Course is searching");
 }
 
-function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-        searchCourses();
-    }
-}
+// function handleKeyPress(event) {
+//     if (event.key === 'Enter') searchCourses();
+// }
